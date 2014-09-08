3d2e5c001b909542c31e5d7a106d485ce6be00d6
nick <nick@nicko.ml>
2014-09-08 19:19:32 +1000

    Add db global

--- board.rb
+++ board.rb
@@ -2,7 +2,9 @@ require 'sinatra'
 require 'sequel'
 
 $config = { :board_name => 'board.rb',
-            :db_url => 'sqlite://board.db'}
+            :db_url => 'sqlite://board.db' }
+
+$db = Sequel.connect $config[:db_url]
 
 get '/' do
   erb :index
