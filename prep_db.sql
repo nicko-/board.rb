14f9325e59925630964beebcbbc22553a8ad771d
nick <nick@nicko.ml>
2014-09-08 19:20:00 +1000

    Add db prep sql

---
+++ prep_db.sql
@@ -0,0 +1,14 @@
+BEGIN TRANSACTION;
+CREATE TABLE `posts` (
+	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	`author`	TEXT NOT NULL,
+	`content`	TEXT NOT NULL,
+	`in_reply_to`	NUMERIC,
+	`tags`	TEXT
+);
+CREATE TABLE `auth` (
+	`client_secret`	TEXT NOT NULL,
+	`server_secret`	TEXT NOT NULL,
+	`hash`	TEXT
+);
+COMMIT;
