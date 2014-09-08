b192c4e50c3a01a09291acd4961e6c33bc4eb534
nick <nick@nicko.ml>
2014-09-08 21:06:46 +1000

    Add date field to post

--- prep_db.sql
+++ prep_db.sql
@@ -1,14 +1,15 @@
 BEGIN TRANSACTION;
 CREATE TABLE `posts` (
-	`id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-	`author`	TEXT NOT NULL,
-	`content`	TEXT NOT NULL,
-	`in_reply_to`	NUMERIC,
-	`tags`	TEXT
+  `id`	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `author`	TEXT NOT NULL,
+  `content`	TEXT NOT NULL,
+  `date` NUMERIC NOT NULL,
+  `in_reply_to`	NUMERIC,
+  `tags`	TEXT
 );
 CREATE TABLE `auth` (
-	`client_secret`	TEXT NOT NULL,
-	`server_secret`	TEXT NOT NULL,
-	`hash`	TEXT NOT NULL
+  `client_secret`	TEXT NOT NULL,
+  `server_secret`	TEXT NOT NULL,
+  `hash`	TEXT NOT NULL
 );
 COMMIT;
