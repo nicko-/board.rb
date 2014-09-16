68d6c3f2264d0f7430ebab014e67df373cdb28e0
nick <nick@nicko.ml>
2014-09-16 19:38:16 +1000

    Tags view now lives at /t/

--- board.rb
+++ board.rb
@@ -167,7 +167,7 @@ get '/th/:id/' do
   erb :thread, :layout => :global
 end
 
-get '/ta/:tags/' do
+get '/t/:tags/' do
   tags = params[:tags].gsub('+', ' ').split.map {|tag| "% #{tag} %"}
   erb :listing, :layout => :global, :locals => {:listing => $db[:posts].where(:in_reply_to => nil).where(Sequel.like(:tags, *tags)).reverse_order(:last_update)}
 end
